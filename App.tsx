f512b54792f9fe3132c6e650003c09184bae7246
Dinesh yadav <dineshyadav74950@gmail.com>
2024-01-04 18:32:45 +0530

    Handling the TextInput

--- App.tsx
+++ App.tsx
@@ -1,118 +1,109 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { ComponentA, ComponentB, ComponentC, Demo, LotsOfStyles } from "./MyComponent";
+import ExStyles from "./style";
+import { useState } from "react";
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+const App = ()=>{
+  const [name,setName] = useState("");
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const [display,setDisplay]=useState(false);
+  const resetData = ()=>{
+    setDisplay(false);
+    setName("");
+    setEmail("");
+    setPassword("");
+  }
+  return(
+    <View>
+      {/* <ComponentA/>
+      <ComponentB/>
+      <ComponentC/> */}
+      {/* <Demo/> */}
+      {/* <LotsOfStyles/> */}
+      {/* <Text style={{fontSize:30, color:'red',backgroundColor:'green'}}>Style in React-Native.</Text>
+      <Text style={styles.textBox}>Style in React-Native.</Text> */}
+      {/* <Text style={ExStyles.textBox}>Style in React-Native.</Text> */}
+      <Text style={[ExStyles.textBox,styles.textBox,{marginTop:10}]}>Style in React-Native..</Text>
+      <Text style={{fontSize:30}}>Your Name : {name}</Text>
+      <TextInput style={style1.inputText} 
+      placeholder="Enter Your Name"
+      value={name}
+      onChangeText={(Text)=>setName(Text)}
+      />
+      <Button title="Clear Input Value" onPress={()=>setName('')}/>
+      <Text style={{fontSize:30}}>Simple form In React-Native</Text>
+      <TextInput
+      style={style2.inputText}
+      onChangeText={(text)=>setName(text)}
+      value={name}
+      placeholder="Enter Your Name"
+      />
+      <TextInput
+      style={style2.inputText}
+      placeholder="enter Your Email Address"
+      onChangeText={(text)=>setEmail(text)}
+      value={email}
+      />
+      <TextInput
+      style={style2.inputText}
+      onChangeText={(text)=>setPassword(text)}
+      value={password}
+      placeholder="Enter Your Password"
+      secureTextEntry = {true}
+      />
+      <View style={{marginBottom:10}}>
+        <Button title="Print Details" color={'cyan'} onPress={()=>setDisplay(true)}/> 
+      </View>
+      <Button title="Clear Details" onPress={()=>resetData()}/>
+      <View>
+        {
+          display ?
+            
+              <View>
+              <Text style={{fontSize:20}}> User name is : {name}</Text>
+              <Text style={{fontSize:20}}> User name is : {email}</Text>
+              <Text style={{fontSize:20}}> User name is : {password}</Text>
+              </View> : null
+        }
+      </View>
+        
     </View>
   );
 }
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  textBox: {
+    color: '#fff',
+    fontSize:25,
+    backgroundColor:'lightblue',
+    marginBottom:10,
+    padding:10,
+    borderRadius:10,
+    height:50,
+    textAlignVertical: 'center',
+    textAlign: 'center',
+    borderColor:'red',
+    borderWidth:2
+  }
+  
 });
+const style1 = StyleSheet.create({
+    inputText:{
+      fontSize:18,
+      color:'whiteblue',
+      borderWidth:2,
+      borderColor:'blue',
+      margin:10
+    }
+  })
+  const style2 = StyleSheet.create({
+    inputText:{
+      fontSize:18,
+      color:'blue',
+      borderWidth:2,
+      borderColor:'blue',
+      margin:10
+    }
+  })
 
 export default App;
---
+++ style.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+ const ExStyles = StyleSheet.create({
+    textBox: {
+        color: '#fff',
+        fontSize: 35,
+        backgroundColor: 'red'
+    }
+});
+export default ExStyles;
